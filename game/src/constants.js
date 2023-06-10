230a8ca15a739d3b2964be96c0eca76c050df442
sethborne <sethborne@gmail.com>
2023-06-10 14:04:11 -0700

    this commit adds two more greetings

--- game/src/constants.js
+++ game/src/constants.js
@@ -5,5 +5,7 @@ const greetings = [
     'It\'s a bird!',
     'It\'s a plane!',
     '1000% more Cube!',
-    'A Whole New Dimension!'
+    'A Whole New Dimension!',
+    'It\'s Cube Tastic',
+    'A Whole New World'
 ];
